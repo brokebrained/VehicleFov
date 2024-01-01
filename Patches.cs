1cdb634fa3ef51fefce2f4b05f20a4597fcdf311
brokebrained <jimmehuk@gmail.com>
2024-01-01 19:20:55 +0000

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Il2Cpp;
+using HarmonyLib;
+
+namespace VehicleFov
+{
+    public class Patches : MelonMod
+    {
+        private static float currentFov = 0f;
+
+        [HarmonyPatch(typeof(vp_FPSCamera), "Start")]
+        private static class GetInitialFov
+        {
+            private static void Postfix(vp_FPSCamera __instance)
+            {
+                currentFov = __instance.m_UnzoomedFieldOfView;
+            }
+        }
+
+        [HarmonyPatch(typeof(Panel_OptionsMenu), "OnConfirmGraphicsOptions")]
+        private static class SettingsChange
+        {
+            private static void Postfix(Panel_OptionsMenu __instance)
+            {
+                float fovSetting = __instance.m_FieldOfViewMin + ((__instance.m_FieldOfViewMax - __instance.m_FieldOfViewMin) * __instance.m_FieldOfViewSlider.value);
+                float aspectRatio = (float)Screen.currentResolution.width / (float)Screen.currentResolution.height;        
+                currentFov = Camera.HorizontalToVerticalFieldOfView(fovSetting, aspectRatio);
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerInVehicle), "EnterVehicle")]
+        private static class SetCurrentFov
+        {
+            private static void Postfix(PlayerInVehicle __instance)
+            {
+                __instance.m_FOV = currentFov;
+            }
+        }       
+    }
+}
